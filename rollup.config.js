1e165630f1ac88db2f728cd2bf9bf483c866bc26
Nicholas Wold <nick@lunadb.io>
2024-07-02 11:08:30 -0700

    get markdown

---
+++ rollup.config.js
@@ -0,0 +1,9 @@
+import {nodeResolve} from "@rollup/plugin-node-resolve"
+export default {
+  input: "./src/codemirror.js",
+  output: {
+    file: "./static/dist/codemirror.bundle.js",
+    format: "iife"
+  },
+  plugins: [nodeResolve()]
+}
---
+++ src/codemirror.js
@@ -0,0 +1,7 @@
+import {EditorView, basicSetup} from "codemirror"
+import {markdown} from "@codemirror/lang-markdown"
+
+let editor = new EditorView({
+  extensions: [basicSetup, markdown()],
+  parent: document.body
+})
