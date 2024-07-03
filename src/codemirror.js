0967e2af06b7c1ae93f5e457bbad9e3a716c0290
Nicholas Wold <nick@lunadb.io>
2024-07-02 17:24:35 -0700

    get two-way syncing mostly working

--- src/codemirror.js
+++ src/codemirror.js
@@ -5,19 +5,21 @@ import {markdown} from "@codemirror/lang-markdown"
 import { DocumentTransaction } from "@lunadb-io/lunadb-client-js";
 
 function syncExtension(initialVersion) {
+    document.getElementById("lastSynced").innerText = "Last synced at: " + initialVersion
+
     let plugin = ViewPlugin.fromClass(class {
         clientId = crypto.randomUUID()
-        version = initialVersion
         destroyed = false
         syncing = false
+        version = initialVersion
 
         constructor(view) {
-            this.view = view;
+            this.view = view
             this.bufferedChanges = this.view.state.changes()
             this.callbackId = setInterval(this.sync.bind(this), 2000)
         }
         update(update) {
-            if (!this.destroyed && update.docChanged) {
+            if (!this.destroyed && update.docChanged && !this.applyingRemote) {
                 this.bufferedChanges = this.bufferedChanges.compose(update.changes)
             }
         }
@@ -26,8 +28,53 @@ function syncExtension(initialVersion) {
             clearInterval(this.callbackId)
         }
         async sync() {
-            let buffer = new DocumentTransaction(this.version, []);
-            this.bufferedChanges.iterChanges((fromA, toA, fromB, toB, inserted) => {
+            if (this.syncing) {
+                return;
+            }
+
+            this.syncing = true;
+            let syncingChanges = this.bufferedChanges
+            this.bufferedChanges = this.view.state.changes()
+            let buffer = this.toTransaction(syncingChanges)
+
+            try {
+                const syncResp = await fetch("/doc", {
+                    method: "PATCH",
+                    headers: {
+                        "content-type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        key: "markdown",
+                        baseTimestamp: buffer.baseTimestamp,
+                        delta: buffer.changes,
+                        sessionId: this.clientId,
+                    })
+                })
+
+                if (syncResp.ok) {
+                    const body = await syncResp.json()
+                    let remoteChangeset = this.toChangeSet(body.changes, revertInFlightSet.newLength)
+                    if (!remoteChangeset.empty) {
+                        // todo: lunadb needs to not return our local changes in its response
+                        this.view.dispatch({changes: remoteChangeset, remote: true})
+                        this.bufferedChanges = this.bufferedChanges.map(remoteChangeset)
+                    }
+                    this.version = body.hlc
+                } else {
+                    console.log("Failed to synchronize: request error", syncResp)
+                    this.bufferedChanges = syncingChanges.compose(this.bufferedChanges)
+                }
+            } catch (e) {
+                console.log("Failed to synchronize: fetch error", e)
+                this.bufferedChanges = syncingChanges.compose(this.bufferedChanges)
+            }
+
+            document.getElementById("lastSynced").innerText = "Last synced at: " + this.version
+            this.syncing = false;
+        }
+        toTransaction(changeset) {
+            let buffer = new DocumentTransaction(this.version, [])
+            changeset.iterChanges((fromA, toA, fromB, toB, inserted) => {
                 if (fromA === toA) {
                     // insertion
                     buffer.stringInsert("/doc", fromA, inserted.toString())
@@ -39,30 +86,20 @@ function syncExtension(initialVersion) {
                     }
                 }
             })
-            if (buffer.changes.length > 0) {
-                try {
-                    const syncResp = await fetch("/doc", {
-                        method: "PATCH",
-                        headers: {
-                            "content-type": "application/json"
-                        },
-                        body: JSON.stringify({
-                            key: "markdown",
-                            baseTimestamp: buffer.baseTimestamp,
-                            delta: buffer.changes,
-                            sessionId: this.clientId,
-                        })
-                    })
-
-                    if (syncResp.ok) {
-                        this.bufferedChanges = this.view.state.changes();
-                    } else {
-                        console.log("Failed to synchronize: request error", syncResp)
+            return buffer
+        }
+        toChangeSet(changes, baseLength) {
+            let changeset = []
+            for (const change of changes) {
+                if (change.pointer === "/doc") {
+                    if (change.op === "stringinsert") {
+                        changeset.push({from: change.idx, insert: change.content})
+                    } else if (change.op === "stringremove") {
+                        changeset.push({from: change.idx, to: change.len})
                     }
-                } catch (e) {
-                    console.log("Failed to synchronize: fetch error", e)
                 }
             }
+            return ChangeSet.of(changeset, baseLength)
         }
     });
     return plugin
