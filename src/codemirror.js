def26fd22387d4312fb28c155b7794213dbacb00
Nicholas Wold <nick@lunadb.io>
2024-07-10 09:42:59 -0700

    prettier

--- src/codemirror.js
+++ src/codemirror.js
@@ -1,147 +1,154 @@
-import {EditorView, basicSetup} from "codemirror"
-import {ViewPlugin} from "@codemirror/view"
-import {ChangeSet} from "@codemirror/state"
-import {markdown} from "@codemirror/lang-markdown"
+import { EditorView, basicSetup } from "codemirror";
+import { ViewPlugin } from "@codemirror/view";
+import { ChangeSet } from "@codemirror/state";
+import { markdown } from "@codemirror/lang-markdown";
 import { DocumentTransaction } from "@lunadb-io/lunadb-client-js";
 
 function syncExtension(initialVersion) {
-    document.getElementById("lastSynced").innerText = "Last synced at: " + initialVersion
+  document.getElementById("lastSynced").innerText =
+    "Last synced at: " + initialVersion;
 
-    return ViewPlugin.fromClass(class {
-        clientId = crypto.randomUUID()
-        destroyed = false
-        syncing = false
-        version = initialVersion
-        applyingUpdates = false
+  return ViewPlugin.fromClass(
+    class {
+      clientId = crypto.randomUUID();
+      destroyed = false;
+      syncing = false;
+      version = initialVersion;
+      applyingUpdates = false;
 
-        constructor(view) {
-            this.view = view
-            this.bufferedChanges = this.view.state.changes()
-            this.callbackId = setInterval(this.sync.bind(this), 2000)
+      constructor(view) {
+        this.view = view;
+        this.bufferedChanges = this.view.state.changes();
+        this.callbackId = setInterval(this.sync.bind(this), 2000);
+      }
+
+      update(update) {
+        if (!this.destroyed && !this.applyingUpdates && update.docChanged) {
+          this.bufferedChanges = this.bufferedChanges.compose(update.changes);
+        }
+      }
+
+      destroy() {
+        this.destroyed = true;
+        clearInterval(this.callbackId);
+      }
+
+      async sync() {
+        if (this.syncing || this.destroyed) {
+          return;
         }
 
-        update(update) {
-            if (!this.destroyed && !this.applyingUpdates && update.docChanged) {
-                this.bufferedChanges = this.bufferedChanges.compose(update.changes)
+        this.syncing = true;
+        let syncingChanges = this.bufferedChanges;
+        this.bufferedChanges = this.view.state.changes();
+        let buffer = this.toTransaction(syncingChanges);
+        let failed = false;
+
+        try {
+          const syncResp = await fetch("/doc", {
+            method: "PATCH",
+            headers: {
+              "content-type": "application/json",
+            },
+            body: JSON.stringify({
+              key: "markdown",
+              baseTimestamp: buffer.baseTimestamp,
+              delta: buffer.changes,
+              sessionId: this.clientId,
+            }),
+          });
+
+          if (syncResp.ok) {
+            const body = await syncResp.json();
+
+            let rawChangeset = this.toChangeSet(body.changes);
+            let remoteChangeset = ChangeSet.of(
+              rawChangeset,
+              syncingChanges.length
+            ).map(syncingChanges, true);
+
+            if (!remoteChangeset.empty) {
+              let remapped = remoteChangeset.map(this.bufferedChanges, true);
+              this.applyChanges(remapped);
+              this.bufferedChanges = this.bufferedChanges.map(remoteChangeset);
             }
+            this.version = body.hlc;
+          } else {
+            console.log("Failed to synchronize: request error", syncResp);
+            failed = true;
+          }
+        } catch (e) {
+          console.log("Failed to synchronize: fetch error", e);
+          failed = true;
         }
 
-        destroy() {
-            this.destroyed = true
-            clearInterval(this.callbackId)
+        if (failed) {
+          document.getElementById("lastSynced").innerText = "Failed to sync!";
+          this.destroy();
+        } else {
+          document.getElementById("lastSynced").innerText =
+            "Last synced at: " + this.version;
         }
+        this.syncing = false;
+      }
 
-        async sync() {
-            if (this.syncing || this.destroyed) {
-                return;
+      applyChanges(changeset) {
+        this.applyingUpdates = true;
+        this.view.dispatch({ changes: changeset, remote: true });
+        this.applyingUpdates = false;
+      }
+
+      toTransaction(changeset) {
+        let buffer = new DocumentTransaction(this.version, []);
+        changeset.iterChanges((fromA, toA, fromB, toB, inserted) => {
+          if (fromA === toA) {
+            buffer.stringInsert("/doc", fromA, inserted.toString());
+          } else {
+            buffer.stringRemove("/doc", fromA, toA - fromA);
+            if (inserted.length > 0) {
+              buffer.stringInsert("/doc", fromA, inserted.toString());
             }
+          }
+        });
+        return buffer;
+      }
 
-            this.syncing = true;
-            let syncingChanges = this.bufferedChanges
-            this.bufferedChanges = this.view.state.changes()
-            let buffer = this.toTransaction(syncingChanges)
-            let failed = false
-
-            try {
-                const syncResp = await fetch("/doc", {
-                    method: "PATCH",
-                    headers: {
-                        "content-type": "application/json"
-                    },
-                    body: JSON.stringify({
-                        key: "markdown",
-                        baseTimestamp: buffer.baseTimestamp,
-                        delta: buffer.changes,
-                        sessionId: this.clientId,
-                    })
-                })
-
-                if (syncResp.ok) {
-                    const body = await syncResp.json()
-
-                    let rawChangeset = this.toChangeSet(body.changes)
-                    let remoteChangeset = ChangeSet.of(rawChangeset, syncingChanges.length).map(syncingChanges, true)
-
-                    if (!remoteChangeset.empty) {
-                        let remapped = remoteChangeset.map(this.bufferedChanges, true)
-                        this.applyChanges(remapped)
-                        this.bufferedChanges = this.bufferedChanges.map(remoteChangeset)
-                    }
-                    this.version = body.hlc
-                } else {
-                    console.log("Failed to synchronize: request error", syncResp)
-                    failed = true
-                }
-            } catch (e) {
-                console.log("Failed to synchronize: fetch error", e)
-                failed = true
+      toChangeSet(changes) {
+        let changeset = [];
+        for (const change of changes) {
+          if (change.pointer === "/doc") {
+            if (change.op === "stringinsert") {
+              changeset.push({ from: change.idx, insert: change.content });
+            } else if (change.op === "stringremove") {
+              changeset.push({ from: change.idx, to: change.idx + change.len });
             }
-
-            if (failed) {
-                document.getElementById("lastSynced").innerText = "Failed to sync!"
-                this.destroy()
-            } else {
-                document.getElementById("lastSynced").innerText = "Last synced at: " + this.version
-            }
-            this.syncing = false;
+          }
         }
-
-        applyChanges(changeset) {
-            this.applyingUpdates = true;
-            this.view.dispatch({changes: changeset, remote: true})
-            this.applyingUpdates = false;
-        }
-
-        toTransaction(changeset) {
-            let buffer = new DocumentTransaction(this.version, [])
-            changeset.iterChanges((fromA, toA, fromB, toB, inserted) => {
-                if (fromA === toA) {
-                    buffer.stringInsert("/doc", fromA, inserted.toString())
-                } else {
-                    buffer.stringRemove("/doc", fromA, toA - fromA)
-                    if (inserted.length > 0) {
-                        buffer.stringInsert("/doc", fromA, inserted.toString())
-                    }
-                }
-            })
-            return buffer
-        }
-
-        toChangeSet(changes) {
-            let changeset = []
-            for (const change of changes) {
-                if (change.pointer === "/doc") {
-                    if (change.op === "stringinsert") {
-                        changeset.push({from: change.idx, insert: change.content})
-                    } else if (change.op === "stringremove") {
-                        changeset.push({from: change.idx, to: change.idx + change.len})
-                    }
-                }
-            }
-            return changeset
-        }
-    })
+        return changeset;
+      }
+    }
+  );
 }
 
 try {
-    const createOrGetResponse = await fetch("/doc", {
-        method: "POST",
-        body: JSON.stringify({ key: "markdown" }),
-        headers: {
-            "content-type": "application/json"
-        }
-    });
-    if (!createOrGetResponse.ok) {
-        throw new Error("Failed to create document");
-    }
-    const body = await createOrGetResponse.json();
-    const hlc = body.hlc;
-    let editor = new EditorView({
-        extensions: [basicSetup, markdown(), syncExtension(hlc)],
-        parent: document.body,
-        doc: body.contents.doc
-    })
+  const createOrGetResponse = await fetch("/doc", {
+    method: "POST",
+    body: JSON.stringify({ key: "markdown" }),
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+  if (!createOrGetResponse.ok) {
+    throw new Error("Failed to create document");
+  }
+  const body = await createOrGetResponse.json();
+  const hlc = body.hlc;
+  let editor = new EditorView({
+    extensions: [basicSetup, markdown(), syncExtension(hlc)],
+    parent: document.body,
+    doc: body.contents.doc,
+  });
 } catch (e) {
-    console.log(e)
-    document.body.innerHTML = e.toString()
+  console.log(e);
+  document.body.innerHTML = e.toString();
 }
--- src/server.js
+++ src/server.js
@@ -1,122 +1,131 @@
-import 'dotenv/config'
-import express from 'express'
-import { v0betaCreateDocument, v0betaDeleteDocument, v0betaGetDocumentContent, v0betaSyncDocument } from '@lunadb-io/lunadb-client-js'
+import "dotenv/config";
+import express from "express";
+import {
+  v0betaCreateDocument,
+  v0betaDeleteDocument,
+  v0betaGetDocumentContent,
+  v0betaSyncDocument,
+} from "@lunadb-io/lunadb-client-js";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(express.static('static'))
-app.use(express.json())
+app.use(express.static("static"));
+app.use(express.json());
 
 let opts = {};
 if (process.env.LUNADB_PASSWD) {
-    opts.basicAuth = process.env.LUNADB_PASSWD
+  opts.basicAuth = process.env.LUNADB_PASSWD;
 }
 
-app.post('/doc', async (req, res) => {
-    const { key } = req.body
-    try {
-        await v0betaCreateDocument(process.env.LUNADB_HOST, key, opts)
-        res.status(201)
-
-        try {
-            await v0betaSyncDocument(
-                process.env.LUNADB_HOST,
-                key,
-                "0",
-                [{
-                    op: "insert",
-                    pointer: "/doc",
-                    content: ""
-                }]
-            )
-        } catch (e) {
-            console.log("Failed to create initial document contents. Document should be recreated!", e)
-            res.status(500).end()
-            return
-        }
-
-        console.log("Document created: " + key)
-    } catch (e) {
-        if (e.status === 409) {
-            res.status(200)
-        } else {
-            console.log("Failed to create document", e)
-            res.status(500).end()
-            return
-        }
-    }
+app.post("/doc", async (req, res) => {
+  const { key } = req.body;
+  try {
+    await v0betaCreateDocument(process.env.LUNADB_HOST, key, opts);
+    res.status(201);
 
     try {
-        const resp = await v0betaGetDocumentContent(process.env.LUNADB_HOST, key, opts)
-        res.json(resp.content)
+      await v0betaSyncDocument(process.env.LUNADB_HOST, key, "0", [
+        {
+          op: "insert",
+          pointer: "/doc",
+          content: "",
+        },
+      ]);
     } catch (e) {
-        res.status(500).end()
+      console.log(
+        "Failed to create initial document contents. Document should be recreated!",
+        e
+      );
+      res.status(500).end();
+      return;
     }
-})
 
-app.delete('/doc', async (req, res) => {
-    const { key } = req.body
-    try {
-        const resp = await v0betaDeleteDocument(process.env.LUNADB_HOST, key, opts)
-        console.log("Document deleted: " + key)
-        res.status(resp.status).end()
-    } catch (e) {
-        console.log(e);
-        if (e.status < 0) {
-            res.status(500).end()
-        } else {
-            res.status(e.status).end()
-        }
+    console.log("Document created: " + key);
+  } catch (e) {
+    if (e.status === 409) {
+      res.status(200);
+    } else {
+      console.log("Failed to create document", e);
+      res.status(500).end();
+      return;
     }
-})
+  }
 
-app.patch('/doc', async (req, res) => {
-    const { key } = req.body
-    let baseTimestamp = undefined;
-    let delta = undefined;
-    let sessionId = undefined;
-    let sessionMetadata = undefined;
-    let fetchAllPresenceData = undefined;
-    if ("baseTimestamp" in req.body) {
-        baseTimestamp = req.body.baseTimestamp;
+  try {
+    const resp = await v0betaGetDocumentContent(
+      process.env.LUNADB_HOST,
+      key,
+      opts
+    );
+    res.json(resp.content);
+  } catch (e) {
+    res.status(500).end();
+  }
+});
+
+app.delete("/doc", async (req, res) => {
+  const { key } = req.body;
+  try {
+    const resp = await v0betaDeleteDocument(process.env.LUNADB_HOST, key, opts);
+    console.log("Document deleted: " + key);
+    res.status(resp.status).end();
+  } catch (e) {
+    console.log(e);
+    if (e.status < 0) {
+      res.status(500).end();
+    } else {
+      res.status(e.status).end();
     }
-    if ("delta" in req.body) {
-        delta = req.body.delta;
+  }
+});
+
+app.patch("/doc", async (req, res) => {
+  const { key } = req.body;
+  let baseTimestamp = undefined;
+  let delta = undefined;
+  let sessionId = undefined;
+  let sessionMetadata = undefined;
+  let fetchAllPresenceData = undefined;
+  if ("baseTimestamp" in req.body) {
+    baseTimestamp = req.body.baseTimestamp;
+  }
+  if ("delta" in req.body) {
+    delta = req.body.delta;
+  }
+  if ("sessionId" in req.body) {
+    sessionId = req.body.sessionId;
+  }
+  if ("sessionMetadata" in req.body) {
+    sessionMetadata = req.body.sessionMetadata;
+  }
+  if ("fetchAllPresenceData" in req.body) {
+    fetchAllPresenceData = req.body.fetchAllPresenceData;
+  }
+  try {
+    const resp = await v0betaSyncDocument(
+      process.env.LUNADB_HOST,
+      key,
+      baseTimestamp,
+      delta,
+      sessionId,
+      "joined",
+      sessionMetadata,
+      fetchAllPresenceData,
+      false,
+      opts
+    );
+    res.status(resp.status).json(resp.content);
+  } catch (e) {
+    console.log(e);
+    if (e.status < 0) {
+      res.status(500).end();
+    } else {
+      res.status(e.status).end();
     }
-    if ("sessionId" in req.body) {
-        sessionId = req.body.sessionId;
-    }
-    if ("sessionMetadata" in req.body) {
-        sessionMetadata = req.body.sessionMetadata;
-    }
-    if ("fetchAllPresenceData" in req.body) {
-        fetchAllPresenceData = req.body.fetchAllPresenceData;
-    }
-    try {
-        const resp = await v0betaSyncDocument(
-            process.env.LUNADB_HOST, 
-            key, 
-            baseTimestamp, 
-            delta, 
-            sessionId,
-            "joined",
-            sessionMetadata, 
-            fetchAllPresenceData, 
-            false, 
-            opts
-        )
-        res.status(resp.status).json(resp.content)
-    } catch (e) {
-        console.log(e);
-        if (e.status < 0) {
-            res.status(500).end()
-        } else {
-            res.status(e.status).end()
-        }
-    }
-})
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
+  console.log(`Serving content at http://localhost:${port}`);
+});
